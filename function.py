4afb8a53146db9534eb81b5f8658b5d3836fb373
Rahul bharti <rahulbhartijk110@gmail.com>
2025-08-20 17:24:42 +0530

    Add files via upload

---
+++ function.py
@@ -0,0 +1,12 @@
+#function definiaton
+
+def avg() :
+    a = int(input("Enter the number :"))
+    b = int(input("Enter the number :"))
+    c = int(input("Enter the number :"))
+
+    avge = (a+b+c)/3
+    print(avge)
+
+avg()   
+    
